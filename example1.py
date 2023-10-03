396c489f161d32bd07d635ae1eacd60655afa251
Daniel Suniaga <danieldsuniaga@gmail.com>
2023-10-03 11:12:51 -0500

    add function is-lead

---
+++ example1.py
@@ -0,0 +1,30 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    
+    n = int(input().strip())
+    
+    if 1<=n and n<=100:
+        
+        if 2<=n and n<=5:
+        
+            print("Weird")
+            
+        elif 6<=n and n<=20:
+            
+            print("Weird")
+            
+        else:
+            
+            print("Not Weird")
+        
+    else: 
+        
+        print("Not weird")    
